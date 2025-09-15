49b71fd5b886d09c06d11ad62ece672c561551e8
Siddant Rachha <siddant.rachha@gmail.com>
2025-09-16 05:07:28 +0530

    bug fix --> prevent api before loading token from storage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,16 +4,23 @@ import FirebaseGoogleAuthHandler from "@/components/FirebaseGoogleAuthHandler";
 import { LoaderOverlay } from "@/components/LoadingOverlay";
 import { GlobalStateProvider } from "@/store/context/Providers/GlobalStateContext";
 import { secureTokenManager } from "@/utils/secure-token-manager/secureTokenManager";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Toast from "react-native-toast-message";
 
 export default function Layout() {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     (async () => {
       await secureTokenManager.loadFromStorageToMemory();
+      setLoading(false);
     })();
   }, []);
+
+  if (loading) return <ActivityIndicator size="large" style={{ flex: 1 }} />;
+
   return (
     <GlobalStateProvider>
       <ConfirmationModal />
